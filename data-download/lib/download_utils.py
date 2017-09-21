64b0119163dc35d9acbe53f079d83d0eef2463e9
Joachim Meyer <j.meyer@utah.edu>
2017-09-21 11:38:21 -0600

    Extent download utils
    Add method the following methods:
    - Ensure a folder exists on the file system.
    - Create a date range for given year, start day of year, end day of year
    
    Minor improvement to the printed ouput of download_file method.

--- data-download/lib/download_utils.py
+++ data-download/lib/download_utils.py
@@ -1,11 +1,35 @@
 import os
 
+from datetime import date, datetime, timedelta
+
 
 def ensure_slash(value):
     if not value.endswith('/'):
         return value + '/'
     else:
         return value
+
+
+def ensure_folder(folder):
+    if not (os.path.isdir(folder)):
+        os.mkdir(folder)
+
+    return folder
+
+
+def dates_in_range(year, from_day, to_day):
+    start_date = datetime(year, 1, 1) + timedelta(from_day - 1)
+    end_date = datetime(year, 1, 1) + timedelta(to_day)
+
+    dates = [
+        date.fromordinal(i)
+        for i in range(start_date.toordinal(), end_date.toordinal())
+    ]
+
+    print('Downloading files from: ' +
+          str(dates[0].strftime("%Y-%m-%d")) + ' to: ' +
+          str(dates[-1].strftime("%Y-%m-%d")))
+    return dates
 
 
 def download_file(session, name, url, download_folder):
@@ -18,8 +42,8 @@ def download_file(session, name, url, download_folder):
             for chunk in response.iter_content(chunk_size=2000000):
                 f.write(chunk)
         if response.status_code is 200:
-            print('Successfully downloaded:' + file_name)
+            print('Successfully downloaded: ' + name)
         else:
-            print('Download for:' + file_name + ' failed')
+            print('Download for: ' + name + ' failed')
     else:
-        print('File:' + file_name + ' already downloaded')
+        print('File: ' + name + ' already downloaded')
