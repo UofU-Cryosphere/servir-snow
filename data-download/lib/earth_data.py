8c0b877e0a6808a84a49cf72101826d825a51333
Joachim Meyer <j.meyer@utah.edu>
2017-10-26 14:28:47 -0600

    Improve EarthData class and initialize with desired satellite source.
    Removes the need to pass the type to each methods of the class and make it an
    attribute.

--- data-download/lib/earth_data.py
+++ data-download/lib/earth_data.py
@@ -6,7 +6,8 @@ from bs4 import BeautifulSoup
 
 class EarthData(requests.Session):
     SOURCES = {
-        'AMSR2': 'https://lance.nsstc.nasa.gov/amsr2-science/data/level3/daysnow/R00/hdfeos5/',
+        'AMSR2': 'https://lance.nsstc.nasa.gov/' +
+                 'amsr2-science/data/level3/daysnow/R00/hdfeos5/',
         'AMSRE': 'https://n5eil01u.ecs.nsidc.org/AMSA/AE_DySno.002/',
     }
 
@@ -15,32 +16,33 @@ class EarthData(requests.Session):
         'AMSRE': re.compile('AMSR_E_L3_DailySnow_V[\d]{2}_.*[.]hdf$'),
     }
 
-    def __init__(self, username, password):
+    def __init__(self, username, password, source):
         super(EarthData, self).__init__()
         self.auth = (username, password)
+        self.source = source
 
-    def get_index(self, source):
-        return self.get(self.SOURCES[source])
+    def get_index(self):
+        return self.get(self.SOURCES[self.source])
 
     # For data sources that have each day in a separate folder
-    def files_for_date_range(self, source, date_range):
+    def files_for_date_range(self, date_range):
         files = {}
 
         for date in date_range:
             folder_name = date.strftime("%Y.%m.%d")
-            index_dir_url = self.SOURCES[source] + folder_name + '/'
+            index_dir_url = self.SOURCES[self.source] + folder_name + '/'
             dir_index = BeautifulSoup(self.get(index_dir_url).text, 'html.parser')
-            file_link = dir_index.find('a', text=self.FILE_REGEX[source])
+            file_link = dir_index.find('a', text=self.FILE_REGEX[self.source])
             files[file_link.attrs['href']] =\
                 index_dir_url + file_link.attrs['href']
 
         return files
 
     # For data sources that have all files in one directory
-    def list_datafiles(self, source):
-        files = BeautifulSoup(self.get_index(source).text, 'html.parser')
-        links = files.find_all(href=self.FILE_REGEX[source])
+    def list_datafiles(self):
+        files = BeautifulSoup(self.get_index().text, 'html.parser')
+        links = files.find_all(href=self.FILE_REGEX[self.source])
         return {
-            link.attrs['href']: self.SOURCES[source] + link.attrs['href']
+            link.attrs['href']: self.SOURCES[self.source] + link.attrs['href']
             for link in links if link is not None
         }
