8618f8463e275a0805563eddc44200ddc52be9ee
Joachim Meyer <j.meyer@utah.edu>
2017-10-26 14:38:42 -0600

    Override requests.Session rebuild_auth method in EarthData class.
    Ensure we keep the auth header when getting redirected to and from the EarthData
    OAuth login.

--- data-download/lib/earth_data.py
+++ data-download/lib/earth_data.py
@@ -2,9 +2,12 @@ import requests
 import re
 
 from bs4 import BeautifulSoup
+from requests.compat import urlparse
 
 
 class EarthData(requests.Session):
+    AUTH_HOST = 'urs.earthdata.nasa.gov'
+
     SOURCES = {
         'AMSR2': 'https://lance.nsstc.nasa.gov/' +
                  'amsr2-science/data/level3/daysnow/R00/hdfeos5/',
@@ -20,6 +23,23 @@ class EarthData(requests.Session):
         super(EarthData, self).__init__()
         self.auth = (username, password)
         self.source = source
+
+    # Overrides from the library to keep headers when redirected to or from
+    # the NASA auth host.
+    def rebuild_auth(self, prepared_request, response):
+        headers = prepared_request.headers
+        url = prepared_request.url
+
+        if 'Authorization' in headers:
+            original_parsed = urlparse(response.request.url)
+            redirect_parsed = urlparse(url)
+
+            if (original_parsed.hostname != redirect_parsed.hostname) and \
+                    redirect_parsed.hostname != self.AUTH_HOST and \
+                    original_parsed.hostname != self.AUTH_HOST:
+                del headers['Authorization']
+
+        return
 
     def get_index(self):
         return self.get(self.SOURCES[self.source])
