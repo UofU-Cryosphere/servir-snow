433296709bce8bcfc5f9004efa339246c812a6ef
Joachim Meyer <j.meyer@utah.edu>
2017-10-26 09:06:34 -0600

    Move logic for merging tiles with GDAL to own class.

--- GDAL/gdal_merge.py
+++ GDAL/gdal_merge.py
@@ -1,11 +1,10 @@
 import click
-import glob
 import os
 import sys
 
 from gdal_calc import gdal_calc
 from osgeo import gdal, osr
-from lib import TileFile, SourceFolder
+from lib import SourceFolder, TileMerger
 
 OUTPUT_FORMAT = 'GTiff'
 
@@ -38,67 +37,6 @@ def filter_band_values(threshold_value):
     gdal_calc(source_file, output_file, threshold_value)
 
 
-def merge_tiles(source_folder):
-    out_file = output_file_name + '.tif'
-
-    # Remove old output file
-    if os.path.isfile(out_file):
-        os.remove(out_file)
-
-    file_queue = []
-    ulx, uly, lrx, lry = 0.0, 0.0, 0.0, 0.0
-    gdal_driver = gdal.GetDriverByName(OUTPUT_FORMAT)
-
-    for file in glob.glob(source_folder + '*[!_proj,_rf].tif'):
-        file = TileFile(file)
-        file_queue.append(file)
-        # Remember dimensions for output file
-        if ulx == 0.0:
-            ulx = file.ulx
-        else:
-            ulx = min(ulx, file.ulx)
-        uly = max(uly, file.uly)
-        lrx = max(lrx, file.lrx)
-        if lry == 0.0:
-            lry = file.lry
-        else:
-            lry = min(lry, file.lry)
-
-    pixel_width = file_queue[0].pixel_width
-    pixel_height = file_queue[0].pixel_height
-
-    bands = file_queue[0].bands
-    band_type = file_queue[0].band_type
-
-    geotransform = [ulx, pixel_width, 0, uly, 0, pixel_height]
-    xsize = int((lrx - ulx) / pixel_width + 0.5)
-    ysize = int((lry - uly) / pixel_height + 0.5)
-
-    output_file = gdal_driver.Create(out_file, xsize, ysize, bands, band_type)
-
-    output_file.SetGeoTransform(geotransform)
-    output_file.SetProjection(file_queue[0].projection)
-
-    fi_processed = 0
-
-    # Copy data from source files into output file.
-    bands_to_copy = range(1, bands+1)
-    for file in file_queue:
-
-        print("Processing file %4d of %4d, %6.3f%% completed"
-              % (fi_processed+1, len(file_queue),
-                 fi_processed * 100.0 / len(file_queue)))
-        # file.report()
-
-        [file.copy_into(output_file, band, band) for band in bands_to_copy]
-
-        fi_processed = fi_processed + 1
-
-        del file
-
-    del output_file
-
-
 @click.command()
 @click.option('--source-folder',
               prompt=True,
@@ -126,7 +64,7 @@ def process_folder(**kwargs):
         file_name = os.path.basename(os.path.dirname(doy_folder))
         output_file_name = doy_folder + file_name
 
-        merge_tiles(doy_folder)
+        TileMerger(doy_folder, output_file_name).create_mosaic()
 
         filter_band_values(kwargs['band_threshold'])
 
--- GDAL/lib/__init__.py
+++ GDAL/lib/__init__.py
@@ -1,7 +1,9 @@
 from .gdal_tile import TileFile
 from .source_folder import SourceFolder
+from .tile_merger import TileMerger
 
 __all__ = [
     'SourceFolder',
-    'TileFile'
+    'TileFile',
+    'TileMerger'
 ]
