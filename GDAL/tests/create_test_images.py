1f81377230b986bfea62b38e9a9b22b60efc84bb
Joachim Meyer <j.meyer@utah.edu>
2018-02-02 09:22:17 -0700

    Update 'create_test_images' script.
    Includes the following changes:
    - Move create logic into own method. This enables importing the script to use
      defined constants.
    - Update tile values to include a lower level limit for fraction of 15. Re-run
      script and added updated tile tiffs.

--- GDAL/tests/create_test_images.py
+++ GDAL/tests/create_test_images.py
@@ -1,7 +1,8 @@
-from osgeo import gdal, gdalnumeric, osr
-
 import numpy
 import os
+import sys
+
+from osgeo import gdal, gdalnumeric, osr
 
 # Utility script to setup tests sources.
 # Creates four tests files that together form a L-shaped mosaic output
@@ -16,10 +17,14 @@ import os
 # For help on forcing values: https://snow.jpl.nasa.gov/portal/data/help_modscag
 
 TILES = {
-    'upper_left.tif': numpy.array([[-999, 0, 0], [10, 20, 100], [100, 2450, 2450]]),
-    'middle.tif': numpy.array([[-999, 0, 0], [10, 200, 400], [400, 2500, 2500]]),
-    'upper_right.tif': numpy.array([[-999, 0, 0], [10, 800, 900], [-10, 2550, 2550]]),
-    'bottom_right.tif': numpy.array([[-999, 0, 0], [-10, -20, 20], [10, 2300, 2300]]),
+    'upper_left.tif':
+        numpy.array([[-999, 0, 15], [10, 20, 100], [100, 2450, 2450]]),
+    'middle.tif':
+        numpy.array([[-999, 0, 0], [10, 200, 400], [400, 2500, 2500]]),
+    'upper_right.tif':
+        numpy.array([[-999, 15, 0], [10, 800, 900], [-10, 2550, 2550]]),
+    'bottom_right.tif':
+        numpy.array([[-999, 0, 0], [-10, -20, 20], [10, 2300, 2300]]),
 }
 geo_transform = {
     'upper_left.tif': [0, 1, 0, 6, 0, -1],
@@ -31,42 +36,47 @@ tile_x = 3
 tile_y = 3
 tile_bands = 1
 
-projection = osr.SpatialReference()
-projection.ImportFromEPSG(4326)
-
-source_folder = 'source_tiffs'
-source_path = os.path.join(
-    os.path.dirname(os.path.abspath(__file__)),
-    source_folder
-)
-
-if not os.path.exists(source_path):
-    os.mkdir(source_path)
-
-
 OUTPUT_FORMAT = 'GTiff'
-gdal_driver = gdal.GetDriverByName(OUTPUT_FORMAT)
+
+PROJECTION = osr.SpatialReference()
+PROJECTION.ImportFromEPSG(4326)
 
 
-for tile_name, tile_values in TILES.items():
-    output_file_name = os.path.join(source_folder, tile_name)
-
-    if os.path.isfile(output_file_name):
-        os.remove(output_file_name)
-
-    output_file = gdal_driver.Create(
-        output_file_name,
-        tile_x, tile_y,
-        tile_bands,
-        gdal.GDT_Int16  # Int16 to enable NoData value of -999
+def create_images():
+    source_folder = 'source_tiffs'
+    source_path = os.path.join(
+        os.path.dirname(os.path.abspath(__file__)),
+        source_folder
     )
 
-    output_file.SetGeoTransform(geo_transform[tile_name])
-    output_file.SetProjection(projection.ExportToWkt())
+    if not os.path.exists(source_path):
+        os.mkdir(source_path)
 
-    target_band = output_file.GetRasterBand(tile_bands)
-    target_band.SetNoDataValue(2550) # From MODSCAG spec
-    gdalnumeric.BandWriteArray(target_band, tile_values)
+    gdal_driver = gdal.GetDriverByName(OUTPUT_FORMAT)
 
-    del target_band
-    del output_file
+    for tile_name, tile_values in TILES.items():
+        output_file_name = os.path.join(source_folder, tile_name)
+
+        if os.path.isfile(output_file_name):
+            os.remove(output_file_name)
+
+        output_file = gdal_driver.Create(
+            output_file_name,
+            tile_x, tile_y,
+            tile_bands,
+            gdal.GDT_Int16  # Int16 to enable NoData value of -999
+        )
+
+        output_file.SetGeoTransform(geo_transform[tile_name])
+        output_file.SetProjection(PROJECTION.ExportToWkt())
+
+        target_band = output_file.GetRasterBand(tile_bands)
+        target_band.SetNoDataValue(2550) # From MODSCAG spec
+        gdalnumeric.BandWriteArray(target_band, tile_values)
+
+        del target_band
+        del output_file
+
+
+if __name__ == '__main__':
+    sys.exit(create_images())
