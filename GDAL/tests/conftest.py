092487b1c930922d1f184b2f6bc1b59d4610ac3e
Joachim Meyer <j.meyer@utah.edu>
2018-02-02 09:46:45 -0700

    Separate out test helper methods to own file in conftest.py
    Clean up the conftest.py and move the helper methods to a dedicated file. This
    also removes the defined constants that now live in the constants file.
    
    Added a utility class OutputFile to test_helper method that makes asserting on
    the script output file easier having pre-parsed most of the information.

--- GDAL/tests/conftest.py
+++ GDAL/tests/conftest.py
@@ -1,51 +1,26 @@
-import datetime
-import gdal_merge
-import os
 import pytest
 import shutil
 
-from click.testing import CliRunner
-
-TEST_ROOT = os.path.dirname(os.path.abspath(__file__))
-TEST_TIFFS = os.path.join(TEST_ROOT, 'source_tiffs')
-TEST_SOURCE_FOLDER = os.path.join(TEST_ROOT, 'source_folder')
+from .constants import TEST_SOURCE_FOLDER
+from .test_helper import create_source_folder, execute_runner, OutputFile
 
 
-def get_cls_marker(cls):
+# Utility method to get a pytest marker from a test class
+def get_cls_marker(cls, name):
     return next(
-        (mark for mark in cls.pytestmark if mark.name == 'runner_args')
+        (mark for mark in cls.pytestmark if mark.name == name)
     )
-
-
-def create_source_folder(name):
-    year = datetime.date.today().year
-    test_source_folder = os.path.join(
-        TEST_SOURCE_FOLDER, str(year), name, str(year) + '001'
-    )
-    if os.path.exists(test_source_folder):
-        shutil.rmtree(test_source_folder)
-
-    shutil.copytree(TEST_TIFFS, test_source_folder)
-
-
-def execute_runner(source_type):
-    arguments =  [
-        '--source-folder', TEST_SOURCE_FOLDER,
-        '--source-type', source_type
-    ]
-
-    cli_runner = CliRunner()
-    return cli_runner.invoke(gdal_merge.process_folder, arguments)
 
 
 @pytest.fixture(scope='class', autouse=True)
 def runner(request):
-    mark = get_cls_marker(request.cls)
+    mark = get_cls_marker(request.cls, 'runner_args')
     source_type = mark.kwargs['source_type']
 
     create_source_folder(source_type)
 
     request.cls.runner = execute_runner(source_type)
+    request.cls.out_file = OutputFile(source_type)
 
     yield
 
---
+++ GDAL/tests/test_helper.py
@@ -0,0 +1,66 @@
+import gdal_merge
+import shutil
+
+from .constants import *
+from click.testing import CliRunner
+from gdal_merge import FILTER_TYPES
+from osgeo import gdal, gdalnumeric, osr
+
+
+def get_source_folder(name):
+    return os.path.join(
+        TEST_SOURCE_FOLDER,
+        RUN_YEAR,
+        FILTER_TYPES[name]['source_folder'],
+        RUN_YEAR + DAY_OF_YEAR
+    )
+
+
+def create_source_folder(name):
+    test_source_folder = get_source_folder(name)
+
+    if os.path.exists(test_source_folder):
+        shutil.rmtree(test_source_folder)
+
+    shutil.copytree(TEST_TIFFS_DIR, test_source_folder)
+
+
+def execute_runner(source_type):
+    arguments = [
+        '--source-folder', TEST_SOURCE_FOLDER,
+        '--source-type', source_type
+    ]
+
+    cli_runner = CliRunner()
+    return cli_runner.invoke(gdal_merge.process_folder, arguments)
+
+
+class OutputFile:
+    def __init__(self, source_type):
+        self.filename = self.get_file_name(source_type)
+        file = gdal.Open(
+            os.path.join(get_source_folder(source_type), self.filename)
+        )
+
+        self.bands = file.RasterCount
+        self.band_type = file.GetRasterBand(1).DataType
+        _raster_band = file.GetRasterBand(1)
+        self.no_data_value = _raster_band.GetNoDataValue()
+        self.band_values = gdalnumeric.BandReadAsArray(_raster_band)
+
+        _projection = osr.SpatialReference(wkt=file.GetProjection())
+        self.projection = _projection.GetAttrValue('geogcs')
+
+        self.xsize = file.RasterXSize
+        self.ysize = file.RasterYSize
+        _geo_transform = file.GetGeoTransform()
+        self.ulx = _geo_transform[0]
+        self.uly = _geo_transform[3]
+        self.pixel_width = _geo_transform[1]
+        self.pixel_height = _geo_transform[5]
+
+        del file
+
+    @classmethod
+    def get_file_name(cls, source_type):
+        return RUN_YEAR + DAY_OF_YEAR + FILTER_TYPES[source_type]['file_suffix']
